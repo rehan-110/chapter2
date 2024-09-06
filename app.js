e41fef5b9e2c146b78cece0fe9777cffee4d22ac
rehan-110 <ar4084480@gmail.com>
2024-09-06 16:07:26 +0500

    chapter2 files

---
+++ app.js
@@ -0,0 +1,24 @@
+//chaper 02
+
+//Q1
+
+var userName , myName = "Ahmed Rehan" , age = "15 Years Old", massage = "Certifies Mobile Application Development";
+alert(myName);
+alert(age);
+alert(massage);
+//Q2-5
+var abc = " PIZZA \n PIZZ \n PIZ \n PI \n P" , email ="ar4084480@gmail.com" ,
+ book ="A smarter way to learn JavaScript";
+
+alert(abc);
+alert(email);
+alert("I am Trying To Learn From The Book "+book);
+
+
+//Q3/8.
+
+document.write("Yah! I can Write HTML Content Through JS")
+
+//Q4/9.
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(design);
